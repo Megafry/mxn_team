8e54bb602f92815e391ae7f045f1b4e153064373
mexanch <mexanch@s32.server.hostpoint.ch>
2020-07-29 16:11:21 +0200

    fix

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -2,6 +2,15 @@
 declare(strict_types = 1);
 
 return [
+  \GeorgRinger\News\Domain\Model\News::class => [
+      'subclasses' => [
+          \Mexan\MxnTeam\Domain\Model\Team::class,
+          \Mexan\MxnTeam\Domain\Model\Client::class,
+          \GeorgRinger\News\Domain\Model\NewsDefault::class,
+           \GeorgRinger\News\Domain\Model\NewsInternal::class,
+           \GeorgRinger\News\Domain\Model\NewsExternal::class,
+      ]
+  ],
   \Mexan\MxnTeam\Domain\Model\Team::class => [
       'tableName' => 'tx_news_domain_model_news',
       'recordType' => \Mexan\MxnTeam\Domain\Model\Team::class,
@@ -12,14 +21,7 @@ return [
       'recordType' => \Mexan\MxnTeam\Domain\Model\Client::class,
       //'recordType' => 3,
   ],
-  \GeorgRinger\News\Domain\Model\News::class => [
-      'tableName' => 'tx_news_domain_model_news',
-      //'recordType' => 0,
-      'subclasses' => [
-          \Mexan\MxnTeam\Domain\Model\Team::class,
-          \Mexan\MxnTeam\Domain\Model\Client::class,
-      ]
-  ],
+
   /*
   \GeorgRinger\News\Domain\Model\NewsInternal::class => [
       'tableName' => 'tx_news_domain_model_news',
